656872d314e6fb7cf70bdaf3af401f7ef8870adf
Smiso-Mdlovu <78770205+Smiso-Mdlovu@users.noreply.github.com>
2021-07-22 14:43:26 +0200

    first commit

---
+++ src/main/java/com/glodintask/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.glodintask.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/glodintask/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,16 @@
+package com.glodintask.employeemanager.repo;
+
+import com.glodintask.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
+
+
